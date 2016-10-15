d0bc82afe849309d829cc043fad2cb3033b80cbe
ikaroz <i@algorhythm.cc>
2016-10-15 14:13:45 +0200

    Cleaned up variable names and put colors into variables.

--- unzip_and_unrar_ebooks.sh
+++ unzip_and_unrar_ebooks.sh
@@ -12,30 +12,38 @@
 # Dependencies: unzip unrar
 # License:      GNU GPL v3.0, see the LICENSE file for more info.
 
+version="0.9.5"
+
 
 
 # Variable assignment
-VERSION="0.9.4"
-TARGET="/home/i/Temp/"                                      ; # Where to place all books
-SKIP1="[._-]([Ee][Pp][Uu][Bb]|[Cc][Oo][Mm][Ii][Cc])[._-]"   ; # Skip EPUB/Comic releases
-SKIP2="\((incomplete|no-nfo|no-sfv|no-sample)\)-"           ; # Skip (incomplete)- (no-nfo)- etc.
-SKIP3="[._-]([Dd][Ii][Rr]|[Nn][Ff][Oo])[Ff][Ii][Xx][._-]"   ; # Skip dirfix/nfofix
+target="/home/i/Temp/"                                      ; # Where to place all books
+skip1="[._-]([Ee][Pp][Uu][Bb]|[Cc][Oo][Mm][Ii][Cc])[._-]"   ; # Skip EPUB/Comic releases
+skip2="\((incomplete|no-(nfo|sfv|sample))\)-"               ; # Skip (incomplete)- (no-nfo)- etc.
+skip3="[._-]([Dd][Ii][Rr]|[Nn][Ff][Oo])[Ff][Ii][Xx][._-]"   ; # Skip dirfix/nfofix
+
+
+
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+bold=$(tput bold)
+reset=$(tput sgr0)
 
 
 
 # Runtime header
-echo ""
-echo -e " \e[1munzip_and_unrar_ebooks.sh v$VERSION (c) 2016 ikaroz <i@algorhythm.cc>\e[0m"
-echo ""
-echo " This program comes with ABSOLUTELY NO WARRANTY. This is free software,"
-echo " and you are welcome to redistribute it under certain conditions."
+header() {
+    echo ""
+    echo -e " ${bold}unzip_and_unrar_ebooks.sh v$version (c) 2016 ikaroz <i@algorhythm.cc>$reset"
+    echo ""
+    echo " This program comes with ABSOLUTELY NO WARRANTY. This is free software,"
+    echo " and you are welcome to redistribute it under certain conditions."
+}
 
 
 
 # Make sure the destination dir exists, otherwise create it
-if [ ! -d "$TARGET" ]; then
-    mkdir "$TARGET"
-fi
+[ ! -d "$target" ] || mkdir "$TARGET"
 
 
 
@@ -45,7 +53,7 @@ if [[ ! ${PWD##*/} =~ ^([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]|[0-9][0-9][0-
 
     echo ""
     echo " We are not in a day dir or month dir:"
-    echo " --> $PWD"
+    echo " $bold--> $PWD$reset"
     echo ""
     echo " The script will now exit."
     echo ""
@@ -56,46 +64,46 @@ fi
 
 
 
-# Using a for loop to go into each dir, unzip stuff into $TARGET, then go into $TARGET and unrar every archive
+# Using a for loop to go into each dir, unzip stuff into $target, then go into $target and unrar every archive
 echo ""
 echo " Starting extraction of .zip files..."
 echo ""
-COUNT=0
-SKIPS=0
-for DIR in $(ls); do
+count=0
+skips=0
+for dir in $(ls); do
 
-    # If $DIR is not a dir, we don't want to try to cd into it...
-    if [ ! -d "$DIR" -o -L "$DIR" ]; then
-        echo "     $DIR"
+    # If $dir is not a dir, we don't want to try to cd into it...
+    if [ ! -d "$dir" -o -L "$dir" ]; then
+        echo "     $dir"
         echo "     \`--> Not a directory, skipping..."
-        ((SKIPS++))
+        ((skips++))
         continue
     fi
 
-    # If $DIR matches any of the skiplists, then... skip it! :)
-    if [[ $DIR =~ $SKIP1 ]]; then
-        echo -e " \e[0;31mx\e[0;00m $DIR - skipping..."
-        ((SKIPS++))
+    # If $dir matches any of the skiplists, then... skip it! :)
+    if [[ $dir =~ $skip1 ]]; then
+        echo -e " ${red}x\e$reset $dir - skipping..."
+        ((skips++))
         continue
-    elif [[ $DIR =~ $SKIP2 ]]; then
-        echo -e " \e[0;31mx\e[0;00m $DIR - skipping..."
-        ((SKIPS++))
+    elif [[ $dir =~ $skip2 ]]; then
+        echo -e " ${red}x$reset $dir - skipping..."
+        ((skips++))
         continue
-    elif [[ $DIR =~ $SKIP3 ]]; then
-        echo -e " \e[0;31mx\e[0;00m $DIR - skipping..."
-        ((SKIPS++))
+    elif [[ $dir =~ $skip3 ]]; then
+        echo -e " ${red}x\e$reset $dir - skipping..."
+        ((skips++))
         continue
     fi
 
-    # Everything checks out -- enter $DIR and do the magic
-    cd $DIR
-    echo -e " \e[0;32mo\e[0;00m $DIR"
+    # Everything checks out -- enter $dir and do the magic
+    cd $dir
+    echo -e " ${green}o$reset $dir"
     # unzip options:
     #   -x file1 file2 *.ext    Exclude these files
     #   -d /path/to/folder/     Destination for extracted files
     #   -L                      Convert extracted files to lowercase
-    unzip "*.zip" -x "file_id.diz" "*.nfo" "*.txt" -d "$TARGET" -L > /dev/null 2>&1
-    ((COUNT++))
+    unzip "*.zip" -x "file_id.diz" "*.nfo" "*.txt" -d "$target" -L > /dev/null 2>&1
+    ((count++))
     cd ..
 
 done
@@ -103,24 +111,24 @@ done
 
 
 echo ""
-echo " All zips have been extracted into \"$TARGET\"."
+echo " All zips have been extracted into \"$target\"."
 echo ""
 echo " Moving on to extraction of .rar archives..."
 echo ""
 
 
 
-# Go to $TARGET and extract all .rar archives, then delete all .rar .r01 .r02 files
-cd "$TARGET"
-for ARCHIVE in $(ls *.rar); do
+# Go to $target and extract all .rar archives, then delete all .rar .r01 .r02 files
+cd "$target"
+for archive in $(ls *.rar); do
 
-    echo -n " $ARCHIVE ... "
+    echo -n " $archive ... "
     # unrar options:
     #   e        Extract files to current directory
     #   -c-      Do not display comments
     #   -inul    Disable all messages
     #   .y       Assume Yes on all queries
-    unrar e -c- -inul -y "$ARCHIVE"
+    unrar e -c- -inul -y "$archive"
     echo "OK"
 
 done
@@ -135,7 +143,7 @@ rm -f *.rar *.r[0-9][0-9] *.txt *.nfo file_id.diz
 
 
 echo ""
-echo -e " \e[1mAll done!\e[0m Processed $COUNT eBook dirs, not counting $SKIPS skipped dirs."
+echo -e " ${bold}All done!$reset Processed $count eBook dirs, not counting $skips skipped dirs."
 
 
 
